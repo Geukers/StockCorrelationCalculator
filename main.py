b2d5a462db665c02ec972e45ccc7ce2c0c713a06
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2020-04-12 11:52:38 -0400

    Added get symbols from excel file

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+import numpy as np
+
+
+def get_symbols():
+    df = pd.read_excel('StockCorrelations.xlsx',
+                       sheet_name='Sheet1')
+    return df['Symbols'].values
+    
+
 if __name__ == '__main__':
-    get_symbols()
-    get_prices()
-    calculate_correlation()
+
+    symbols = get_symbols()
+    # get_prices(symbols)
+    # calculate_correlation()
 
