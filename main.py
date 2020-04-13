31b6a8ed70bbf426ad23ac3133352ca93eb5daa6
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2020-04-12 22:47:51 -0400

    Added print graph correlations

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     period1_unix_timestamp = repr(int(period1_date_time.replace(tzinfo=timezone.utc).timestamp()))
 
     daily_stock_price = pd.DataFrame()
-
+    print("\n===================== Errors =======================\n")
     for stock in df['Symbols']:
         CSV_URL = 'https://query1.finance.yahoo.com/v7/finance/download/' + stock + \
                   '?period1=' + period1_unix_timestamp + \
@@ -38,11 +38,53 @@ if __name__ == '__main__':
 
             close_price_list = []
 
+
             for close_price in my_list:
                 close_price_list.append(close_price[4])
+            try:
+                daily_stock_price[stock] = close_price_list
+            except ValueError:
+                print(stock + ' has not been public long enough, it has been removed')
 
-            daily_stock_price[stock] = close_price_list
+    print()
+    # i1 = 0
+    # while i1 < len(daily_stock_price.columns) - 1:
+    #     col1 = daily_stock_price.iloc[:, i1]
+    #     i2 = i1 + 1
+    #     while i2 < len(daily_stock_price.columns):
+    #         col2 = daily_stock_price.iloc[:, i2]
+    #         print(daily_stock_price.columns[i1] + " : " + daily_stock_price.columns[i2])
+    #         print(repr(np.corrcoef(np.array(col1).astype(np.float), np.array(col2).astype(np.float))))
+    #         i2 = i2 + 1
+    #     i1 = i1 + 1
 
+    # print(daily_stock_price)
+
+    # print(np.array(daily_stock_price))
+
+    print("\n================ Graph Correlations ================\n")
+
+    array = np.vstack((daily_stock_price.iloc[:, 0].astype(np.float), (daily_stock_price.iloc[:, 1].astype(np.float))))
+    # print(array)
+
+    for i in range(2, len(daily_stock_price.columns)):
+
+        # print((daily_stock_price[col]).astype(np.float))
+        # print('askdjhfasdfkljhasdfhjklfasdkhjlafsdhkjladfshkjlfskhdjl')
+        # array = np.array([array], (daily_stock_price[col]).astype(np.float).tolist())
+
+        array = np.vstack((array, daily_stock_price.iloc[:, i].astype(np.float).tolist()))
+
+        # array = array[..., daily_stock_price.iloc[:, i].astype(np.float).tolist()]
+
+
+    # print(array)
+
+    print(daily_stock_price.columns.tolist())
+    correlation = np.corrcoef(array)
+    print(correlation)
+
+    print("\n============ Individual Correlations ==============\n")
     i1 = 0
     while i1 < len(daily_stock_price.columns) - 1:
         col1 = daily_stock_price.iloc[:, i1]
@@ -54,14 +96,12 @@ if __name__ == '__main__':
             i2 = i2 + 1
         i1 = i1 + 1
 
-    # array = np.corrcoef(np.array(daily_stock_price).astype(np.float))
-    #
     # workbook = xlsxwriter.Workbook('StockCorrelations.xlsx')
     # worksheet = workbook.add_worksheet(name="Correlations")
     #
     # row = 0
     #
-    # for col, data in enumerate(array):
+    # for col, data in enumerate(correlation):
     #     worksheet.write_column(row, col, data)
     #
     # workbook.close()
